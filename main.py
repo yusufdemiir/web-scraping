712fa46233f4f4f9a6d8d8c27a6fe93fbcc05cec
yusufdemiir <demir254yusuf@gmail.com>
2025-03-19 16:50:40 +0300

    git ignore

--- main.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('index.html', 'r') as html_file:
-    content = html_file.read()
-
-    soup = BeautifulSoup(content, 'lxml')
-    main_headings = soup.find_all('h2')
-    for heading in main_headings:
-        print(heading.text)
