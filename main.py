3c031796dddf6196713b4e00b8dac7f61f0109d7
yusufdemiir <demir254yusuf@gmail.com>
2025-03-19 15:16:10 +0300

    Scraping all h2 tags as a text.

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from bs4 import BeautifulSoup
 
 with open('index.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+    main_headings = soup.find_all('h2')
+    for heading in main_headings:
+        print(heading.text)
